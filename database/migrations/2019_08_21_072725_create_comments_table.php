f7266c9fb47bf562abe144c4c7d4e0dde8a087ae
unknown <nicolash@fusion-solutions.com.au>
2019-08-23 17:27:38 +0930

    implement db schema

--- database/migrations/2019_08_21_072725_create_comments_table.php
+++ database/migrations/2019_08_21_072725_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('body');
+            $table->integer('commentable_id');
+            $table->string('commentable_type');
             $table->timestamps();
         });
     }
--- database/migrations/2019_08_23_064804_add_column_to_user_table.php
+++ database/migrations/2019_08_23_064804_add_column_to_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddColumnToUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            
+            $table->integer('country_id');
         });
     }
 
@@ -26,6 +26,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
     }
 }
